a7b2bc3303f1812be96b9426dbd9a506e58926be
Leo <leolima29600@gmail.com>
2025-10-11 22:49:06 -0300

    acomodo static para que sirva en todas las apps. creo dashboard front

--- frontend/frontend/urls.py
+++ frontend/frontend/urls.py
@@ -2,13 +2,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from usuarios import views 
+from dashboard import views as dashboard_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     
-
     # Rutas de Frontend (usando las vistas importadas de 'usuarios')
     path('inicio/', views.inicio_view, name='inicio'),
     path('miperfil/', views.perfil_view, name='miperfil'),
+    path('dashboard/', dashboard_views.dashboard_view, name='dashboard.html'), 
 ]
