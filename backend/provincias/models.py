79550f77bc82ad8b11d2ce2828655780e4267da0
Santiago Gerber <gerbersan7@gmail.com>
2025-10-15 21:30:31 -0300

    Cambios

--- backend/provincias/models.py
+++ backend/provincias/models.py
@@ -2,14 +2,7 @@ from django.db import models
 
 
 class Provincia(models.Model):
-<<<<<<< HEAD
     nombre = models.CharField(max_length=100, unique=True) #unique=True para que no se repitan nombres
 
     def __str__(self):
         return self.nombre
-=======
-    nombre = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.nombre  
->>>>>>> main
