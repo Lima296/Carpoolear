2583a88b86d467100ab19aa0477f6989b6be0fe5
Marcos <marcosgamba49@gmail.com>
2025-11-12 18:40:55 -0300

    Ya no se que poner

--- frontend/usuarios/urls.py
+++ frontend/usuarios/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('inicio/', views.inicio_view, name='inicio'),  
+    path('inicio/', views.inicio_view, name='inicio'),
+    path('ayuda/', views.ayuda_view, name='ayuda'),
     ]
--- frontend/usuarios/views.py
+++ frontend/usuarios/views.py
@@ -8,3 +8,6 @@ def perfil_view(request):
 
 def perfil_publico_view(request, usuario_id):
     return render(request, 'perfil_publico.html', {'usuario_id': usuario_id})
+
+def ayuda_view(request):
+    return render(request, 'ayuda.html')
