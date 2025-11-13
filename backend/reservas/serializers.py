9d732f0cff7750cf9dcb4ebf3018677998fb12d6
Leo <leolima29600@gmail.com>
2025-11-12 21:03:10 -0300

    detalles errores menores

--- backend/reservas/serializers.py
+++ backend/reservas/serializers.py
@@ -1,12 +1,16 @@
 from rest_framework import serializers
 from .models import Reserva
+from viajes.serializers import ViajeSerializer
+from usuarios.serializers import UsuarioSerializer
 
 # Serializer para LEER datos (incluye detalles anidados)
 class ReservaReadSerializer(serializers.ModelSerializer):
+    viaje = ViajeSerializer(read_only=True)
+    usuario = UsuarioSerializer(read_only=True)
+
     class Meta:
         model = Reserva
-        fields = '__all__'
-        depth = 2
+        fields = ['uuid', 'usuario', 'viaje', 'fecha_reserva', 'cantidad_asientos', 'estado']
 
 # Serializer para ESCRIBIR datos (usa IDs para relaciones)
 class ReservaWriteSerializer(serializers.ModelSerializer):
