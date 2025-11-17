7c3c822829bbcbcb0cbaafdc69dcaa2652b2010e
Leo <leolima29600@gmail.com>
2025-11-17 19:42:35 -0300

    Arreglos errores menores

--- frontend/dashboard/views.py
+++ frontend/dashboard/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+from datetime import date
 
 def dashboard_view(request):
-    return render(request, 'dashboard.html')
+    today = date.today().isoformat()  # Get today's date in YYYY-MM-DD format
+    return render(request, 'dashboard.html', {'current_date': today})
 
 def viaje_editar_view(request, viaje_id):
-    return render(request, 'viaje_editar.html', {'viaje_id': viaje_id})
+    today = date.today().isoformat()
+    return render(request, 'viaje_editar.html', {'viaje_id': viaje_id, 'current_date': today})
 
 def misviajes_view(request):
-    return render(request, 'misviajes.html')
+    today = date.today().isoformat()
+    return render(request, 'misviajes.html', {'current_date': today})
