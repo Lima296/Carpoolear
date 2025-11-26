f6891ac54b6c442543cf35d6eca7c6cc76438164
Marcos <marcosgamba49@gmail.com>
2025-11-26 18:32:07 -0300

    feat(auth): Proteger vistas y redirigir al inicio de sesión

--- frontend/dashboard/views.py
+++ frontend/dashboard/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
 from datetime import date
 
+from django.contrib.auth.decorators import login_required
+
+@login_required
 def dashboard_view(request):
     today = date.today().isoformat()  # Get today's date in YYYY-MM-DD format
     return render(request, 'dashboard.html', {'current_date': today})
 
+@login_required
 def viaje_editar_view(request, viaje_id):
     today = date.today().isoformat()
     return render(request, 'viaje_editar.html', {'viaje_id': viaje_id, 'current_date': today})
 
+@login_required
 def misviajes_view(request):
     today = date.today().isoformat()
     return render(request, 'misviajes.html', {'current_date': today})
--- frontend/usuarios/views.py
+++ frontend/usuarios/views.py
@@ -1,13 +1,16 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 def inicio_view(request):
     return render(request, 'inicio.html')
 
+@login_required
 def perfil_view(request):
     return render(request, 'miperfil.html')
 
 def perfil_publico_view(request, usuario_id):
     return render(request, 'perfil_publico.html', {'usuario_id': usuario_id})
 
+@login_required
 def ayuda_view(request):
     return render(request, 'ayuda.html')
